1dc4d06a486dfb2cb2996ead85b9f1b9858ce09a
FahadFayyaz <m.fahadmun@gmail.com>
2023-09-18 12:13:33 +0500

    first chapter done

---
+++ Assignment.js
@@ -0,0 +1,18 @@
+// ***********************************Chapter 1 **********************************
+// ***********************************Q1 **********************************
+var firstName="Fahad"
+var lastName="Fayyaz"
+var email="m.fahadmun@gmal.com"
+var phoneNumber ="03040246913"
+var password="abc123"
+
+alert("First name is: " + firstName);
+alert("Last name is: " + lastName);
+alert("email is: " + email);
+alert("phone number is: " + phoneNumber);
+alert("password is: " + password);
+
+// ***********************************Q2 **********************************
+alert("You're learning JavaScript!");
+// ***********************************Q3 **********************************
+alert("A cup of coffee and javascript is best therapy ;)");
